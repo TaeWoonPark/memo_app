b24c524edbf63315fd34af1d9b3a8c3a7fcd3f9a
Tae Woon Park <yasuo19@gmail.com>
2025-10-11 15:11:01 +0900

    メモの詳細機能作成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use LiveWire\Volt\Volt;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// 詳細ページ
+volt::route('/memos/{memo}', 'memos.show')->name('me mos.show');
